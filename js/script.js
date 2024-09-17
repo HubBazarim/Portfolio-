f2e115002071d17b1707bdc71c5a7999ff11331a
HubBazarim <166938639+HubBazarim@users.noreply.github.com>
2024-09-17 15:04:25 -0300

    adaptado para celular e notebooks, menu hamburguer corrigido e tamanho de pdfs

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
+const menuHamburguer = document.querySelector(".menu-hamburguer");
 
-/* tur the menu hamburguer into a x ---------*/
+menuHamburguer.addEventListener('click', toggleMenu);
 
-const menuHamburguer = document.querySelector (".menu-hamburguer");
-menuHamburguer .addEventListener ('click', () => {
-  console.log("OK");    //LOG
-});
+function toggleMenu() {
+  const nav = document.querySelector('.nav-responsive');
+  console.log('Menu toggle', nav);
+  menuHamburguer.classList.toggle('change');
 
-function toggleMenu () {
-const nav = document. querySelector('.nav-responsive');
-console.log(nav); //LOG
-menuHamburguer.classList.toggle('change');
-
-if (menuHamburguer.classList.contains('change')) {
-  nav.style.display = 'block';
-
-  
-}
-
-
+  if (menuHamburguer.classList.contains('change')) {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
+  }
 }
